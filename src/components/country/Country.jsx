22b72e5cc0eda0ee6d54de588f330da197655abc
Shafkatahmad <shafkataahmad@gmail.com>
2026-01-04 15:46:40 +0600

    handle state from different file

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './country.css';
-const Country = ({country}) => {
-  console.log(country)
+const Country = ({country, handleVisitedCountries}) => {
+  // console.log(country)
 
   const [visited, setVisited] = useState(false);
 
@@ -9,12 +9,17 @@ const Country = ({country}) => {
     setVisited(!visited);
   }
 
+  // const passWithParams = () => handleVisitedCountries(country);
+  
+
   const {name, flags, cca3} = country;
   return (
     <div className={`country ${visited && 'visited'}`}>
-      <h3>Name: {name?.common}</h3>
+      <h3 style={{color: visited? 'purple': 'white'}}>Name: {name?.common}</h3>
       <img className='img' src={flags.svg} alt="" />
       <p><small>Code: {cca3}</small></p>
+      <button onClick={() => handleVisitedCountries(country)}>Mark Visited</button>
+      <br />
       <button onClick={handleVisited}>{visited? 'visited': 'going'}</button>
       {visited ? 'I have visited this country': 'I want to visit'}
     </div>
